d8ed2c8283152e177320765b9c81423d532a3048
upender.ramidi.urr@gmail.com <upend@LAPTOP-AH82UCUT>
2024-03-07 16:44:43 +0530

    Hooks Commit

--- CucumberTestNG/src/test/java/stepdefinitions/LoginStepDefinitions.java
+++ CucumberTestNG/src/test/java/stepdefinitions/LoginStepDefinitions.java
@@ -1,17 +1,34 @@
 package stepdefinitions;
 
 import org.openqa.selenium.By;
+import org.openqa.selenium.OutputType;
+import org.openqa.selenium.TakesScreenshot;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 
+import io.cucumber.java.After;
+import io.cucumber.java.Scenario;
 import io.cucumber.java.en.Given;
 import io.cucumber.java.en.Then;
 import io.cucumber.java.en.When;
 
 public class LoginStepDefinitions {
-
+	
 	WebDriver driver;
 
+	@After
+	public void takeScreenshotOnFailure(Scenario scenario) {
+
+	if (scenario.isFailed()) {
+
+	TakesScreenshot ts = (TakesScreenshot) driver;
+
+	byte[] src = ts.getScreenshotAs(OutputType.BYTES);
+	scenario.attach(src, "image/png", "screenshot");
+	}
+
+	}
+	
 	@Given("User Opens the Browser")
 	public void user_opens_the_browser() {
 		System.setProperty("webdriver.chrome.driver",
@@ -23,7 +40,7 @@ public class LoginStepDefinitions {
 	@When("Navigates to Google Page")
 	public void navigates_to_google_page() {
 		driver.get("https://www.google.com");
-		//driver.findElement(By.id("Name")).sendKeys("Name");
+		driver.findElement(By.id("Name")).sendKeys("Name");
 	}
 
 	@Then("Get the Title of the page")
